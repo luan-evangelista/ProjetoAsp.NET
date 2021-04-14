06331584775a264ee73ca228a4f5f48227be41f5
Luan Evangelista <luan.evieira43@gmail.com>
2021-04-14 10:52:21 -0300

    Estilo Pág.: Sobre Mim

---
+++ src/FilmesCRUDRazor/Pages/Sobre.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace FilmesCRUDRazor.Pages
+{
+    public class SobreModel : PageModel
+    {
+
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
